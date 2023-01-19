e6f76111f87e696d24c4a20d4703ec42022a5418
Juan Hunter <jhunter@unal.edu.co>
2023-01-19 14:52:12 -0500

    ApiReactConnection

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import axios from 'axios';
+import users from './components/user';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hola mundo</h1>
+    </div>
+  );
+}
+
+export default App;
