436944dfa3dee66ee1ff92b715bc8e0d447b9039
srestrero <srestreporo@unal.edu.co>
2023-01-25 10:27:28 -0500

    add redux and stabilish comunication with rails

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,31 @@
 import './App.css';
-import Navbar from './components/navbar';
-import ProfileHome from './components/profile/profileHome';
-import AdminHome from './components/adminProfile/adminhome';
+// import Navbar from './components/navbar';
+// import ProfileHome from './components/profile/profileHome';
+// import AdminHome from './components/adminProfile/adminhome';
+import LoginScreen from './components/authScreens/LogInScreen';
+// function chooseUser(userList, currentId){
+//   let currentUser=userList.filter(userList => userList.id == currentId)
+//   return JSON.stringify(currentUser)
+// }
 
-function chooseUser(userList, currentId){
-  let currentUser=userList.filter(userList => userList.id == currentId)
-  return JSON.stringify(currentUser)
-}
+// const USER = {id: 1, role:false, name: "Messi", email: "MESSI@gmail.com", englishLevel: "A2", knowledge: "FULBO MUCHO FULBO", linkcv: "link", image: "https://img.a.transfermarkt.technology/portrait/big/28003-1671435885.jpg?lm=1"}
 
-function itsadmin(userInfo){
-  return userInfo.role ? true : false;
-}
-
+// function itsadmin(userInfo){
+//   return userInfo.role ? true : false;
+// }
 
 function App() {
   return (
-    <div>
-      {/* {console.log(chooseUser(USERS,2))} */}
-      <Navbar/>
-      <br/>
-      {}
-      {itsadmin(USER) ? <AdminHome userInfo={USER}/> : <ProfileHome userInfo={USER}/>}
-      
-    </div>
+    // <div>
+    //   {/* {console.log(chooseUser(USERS,2))} */}
+    //   <Navbar/>
+    //   <br/>
+    //   {}
+    //   {itsadmin(USER) ? <AdminHome userInfo={USER}/> : <ProfileHome userInfo={USER}/>}
+    //
+    // </div>
+      <LoginScreen/>
   );
 }
-
-const USER = {id: 1, role:false, name: "Messi", email: "MESSI@gmail.com", englishLevel: "A2", knowledge: "FULBO MUCHO FULBO", linkcv: "link", image: "https://img.a.transfermarkt.technology/portrait/big/28003-1671435885.jpg?lm=1"}
-
-
-
 
 export default App;
